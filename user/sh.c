f27d0e467b4c61f378e053564df6150f3a0d5dd4
Wenhao Tang <thwfhk@163.com>
2020-12-22 00:47:36 -0800

    lab 5 exercise 10

--- user/sh.c
+++ user/sh.c
@@ -55,7 +55,12 @@ again:
 			// then close the original 'fd'.
 
 			// LAB 5: Your code here.
-			panic("< redirection not implemented");
+			int fd = open(t, O_RDONLY);
+			if (fd < 0) panic("input redirection error %e", fd);
+			if (fd != 0) {
+				dup(fd, 0);
+				close(fd);
+			}
 			break;
 
 		case '>':	// Output redirection
